7eed89e70a345f5e7a50de1597d4bf17e2fdab64
Adnan Al-farizi Rahmat <adnanarahmat45@gmail.com>
2022-03-04 11:14:19 +0700

    fix : url home

--- assets/components/header.js
+++ assets/components/header.js
@@ -6,10 +6,10 @@ class NavigationBar extends HTMLElement {
                 <div class="container">
                     <nav>
                         <div class="nav__brand">
-                            <a href=""><h1>My Movie</h1></a>
+                            <a href="index.html"><h1>My Movie</h1></a>
                         </div>
                         <div class="nav__menu">
-                            <a href="" class="nav__menu__link">Home</a>
+                            <a href="index.html" class="nav__menu__link">Home</a>
                             <a href="popular.html" class="nav__menu__link">Popular</a>
                             <a href="top_rated.html" class="nav__menu__link">Top Rated</a>
                             <a href="upcoming.html" class="nav__menu__link">Upcoming</a>
